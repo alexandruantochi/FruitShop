04b5975cf474dad8f6f56612f126fdac9001a107
alexandruantochi <alexandru.antochi@gmail.com>
2016-04-10 04:28:27 +0300

    Started working on Users

--- src/eu/ubis/eshop/bf/integration/model/UserEntity.java
+++ src/eu/ubis/eshop/bf/integration/model/UserEntity.java
@@ -1,5 +1,49 @@
 package eu.ubis.eshop.bf.integration.model;
 
 public class UserEntity {
+	
+	private String name;
+	private String first_name;
+	private String adress;
+	private String email;
+	private String phone;
+	
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
 }
--- src/eu/ubis/eshop/bf/integration/repo/UserDAOBeen.java
+++ src/eu/ubis/eshop/bf/integration/repo/UserDAOBeen.java
@@ -1,5 +1,58 @@
 package eu.ubis.eshop.bf.integration.repo;
 
-public class UserDAOBeen {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
+import eu.ubis.eshop.bf.integration.model.UserEntity;
+
+public class UserDAOBeen {
+	private static final String GET_USER_DETAILS = "SELECT * FROM `users` WHERE `username`=? AND `password`=?";
+	private static final String GET_USER_ORDERS = "SELECT `orderID` FROM `orders` where `userID`=?";
+
+
+	public List<UserEntity> getUserDetails() {
+
+		Connection con = ConnectionHelperClass.getMysqlConnection();
+		List<UserEntity> userEntities = new ArrayList<UserEntity>();
+
+		try {
+			ResultSet resultSet = con.createStatement().executeQuery(GET_USER_DETAILS);
+			while (resultSet.next()) {
+				UserEntity userEntity = new UserEntity();
+				userEntity.setId(resultSet.getInt("id"));
+				userEntity.setName(resultSet.getString("name"));
+				userEntity.setFirst_name(resultSet.getString("first_name"));
+				userEntity.setAdress(resultSet.getString("adress"));
+				userEntity.setEmail(resultSet.getString("email"));
+				userEntity.setPhone(resultSet.getString("phone"));
+				userEntities.add(userEntity);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		return userEntities;
+	}
+
+	public List<Integer> getUserOrders(int id) {
+		Connection con = ConnectionHelperClass.getMysqlConnection();
+		List<Integer> orderList = new ArrayList<Integer>()
+		try {
+			PreparedStatement prepareStatement = con.prepareStatement(GET_USER_ORDERS);
+			prepareStatement.setInt(1, id);
+			ResultSet resultSet = prepareStatement.executeQuery();
+			while (resultSet.next())
+				orderList.add(resultSet.getInt("orderID"));
+			return orderList;
+		
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		return (new ArrayList<Integer>());
+	}
 }
